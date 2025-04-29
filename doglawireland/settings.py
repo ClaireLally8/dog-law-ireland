03e835e90e8762255be6ab601bd710961b32d725
Claire Lally <clairelally@Claires-MacBook-Pro-2.local>
2025-04-29 20:36:59 +0100

    Configure static root

--- doglawireland/settings.py
+++ doglawireland/settings.py
@@ -124,11 +124,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
