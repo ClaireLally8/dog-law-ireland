69db9465d8f190ca41f5e8d6dc75f5e807f69d0e
Claire Lally <clairelally@Claires-MacBook-Pro-2.local>
2025-04-29 19:16:16 +0100

    Created procfile

--- env.py
+++ env.py
@@ -1,3 +1,3 @@
 import os
 
-os.environ.setdefault('DJANGO_SECRET_KEY', "django-insecure-5$3k1!n$h92a@v6+7!0u^&z%b_3vx7wqzq^3@-r^v2l0tx9g4!")
+os.environ.setdefault('DJANGO_SECRET_KEY', "-u2vpoxdy9v!z(27+7nqfsf)ykhu!w1t56eug-%hfoskzf1q3^")
