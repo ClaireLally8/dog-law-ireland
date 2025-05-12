0f5652e38158cadd2108e36a6868df7abe41a7cd
Claire Lally <claire@Claires-MacBook-Pro.local>
2025-05-12 21:36:59 +0100

    deleted env.py

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ.setdefault('DJANGO_SECRET_KEY', "-u2vpoxdy9v!z(27+7nqfsf)ykhu!w1t56eug-%hfoskzf1q3^")
-os.environ.setdefault('DATABASE_URL', "postgres://ueqistjht9u8ek:p0052df3b4edf8aeedf8ed85e8eb14c18638e17c9e26884a530ac644eda959ff8@cek6h0n4ffe7ur.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/dast3fd7o4lu32")
