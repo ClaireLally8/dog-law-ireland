301ef2c0474477eb0ee893ea4ed2a2ccc1cd0080
Claire Lally <claire@Claires-MacBook-Pro.local>
2025-05-20 19:13:32 +0100

    Updated debug

--- doglawireland/settings.py
+++ doglawireland/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
