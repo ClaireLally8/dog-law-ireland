4f3fb4d2fb0ab5767a4418d4bddb9f9a7181f8ec
Claire Lally <claire@Claires-MacBook-Pro.local>
2025-05-20 22:36:35 +0100

    Debug false

--- doglawireland/settings.py
+++ doglawireland/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['"https://web-production-eff60.up.railway.app','*',]
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
