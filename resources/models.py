553222343bdce631217eb0e55d5cd8b54fbb4361
Claire Lally <claire@Claires-MacBook-Pro.local>
2025-05-25 22:10:14 +0100

    Add slug

--- resources/models.py
+++ resources/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 class Resource(models.Model):
-    slug = models.SlugField(unique=True, blank=True)
+    slug = models.SlugField(blank=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     tagline = models.TextField(blank=True, null=True)
@@ -18,8 +18,16 @@ class Resource(models.Model):
         if self.file:
             if not self.file.name.lower().endswith('.pdf'):
                 raise ValidationError('Uploaded file must be a PDF.')
+    
+    def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.name)
-
+            base_slug = slugify(self.name or "resource")
+            slug = base_slug
+            counter = 1
+            while Resource.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)   
     def __str__(self):
         return self.name
