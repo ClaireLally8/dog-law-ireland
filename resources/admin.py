96cf4da9789a233c8555836b83099dad18a6eb4b
Claire Lally <claire@e2:8f:79:64:df:9a.home>
2025-06-15 22:16:01 +0100

    Resources restyle

--- resources/admin.py
+++ resources/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Resource
+from .models import Resource, ResourceImage, ResourceLink
 
+class ResourceImageInline(admin.TabularInline):
+    model = ResourceImage
+    extra = 1
+    fields = ('image', 'is_banner', 'is_thumbnail')
 
+class ResourceLinkInline(admin.TabularInline):
+    model = ResourceLink
+    extra = 1
+    fields = ('name', 'url')
+
+@admin.register(Resource)
 class ResourceAdmin(SummernoteModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
+    list_display = ('name', 'slug', 'uploaded_at')
+    prepopulated_fields = {'slug': ('name',)}
     summernote_fields = ('description',)
+    inlines = [ResourceImageInline, ResourceLinkInline]
 
-admin.site.register(Resource, ResourceAdmin)
--- resources/models.py
+++ resources/models.py
@@ -35,3 +35,41 @@ class Resource(models.Model):
         super().save(*args, **kwargs)   
     def __str__(self):
         return self.name
+
+class ResourceLink(models.Model):
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE, related_name='links', null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.url})"
+
+class ResourceImage(models.Model):
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='uploads/', null=True, blank=True)
+    is_banner = models.BooleanField(default=False, null=True, blank=True)
+    is_thumbnail = models.BooleanField(default=False, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        # Ensure only one banner per resource
+        if self.is_banner:
+            ResourceImage.objects.filter(resource=self.resource, is_banner=True).exclude(pk=self.pk).update(is_banner=False)
+        else:
+            # If no other image is marked banner, set this one
+            if not self.resource.images.exclude(pk=self.pk).filter(is_banner=True).exists():
+                self.is_banner = True
+                self.save(update_fields=['is_banner'])
+
+        # Ensure only one thumbnail per resource
+        if self.is_thumbnail:
+            ResourceImage.objects.filter(resource=self.resource, is_thumbnail=True).exclude(pk=self.pk).update(is_thumbnail=False)
+        else:
+            # If no other image is marked thumbnail, set this one
+            if not self.resource.images.exclude(pk=self.pk).filter(is_thumbnail=True).exists():
+                self.is_thumbnail = True
+                self.save(update_fields=['is_thumbnail'])
+
+    def __str__(self):
+        return f"Image for {self.resource.name}"
